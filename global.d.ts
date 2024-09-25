2e43d7ff59b85e31421b1031ac30fce5978072db
WitchDevelops <d.n.wojewska@gmail.com>
2024-09-25 23:30:12 +0200

    fix: global types

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  type NameMap = {
-    [key: string]: string;
-  };
-  type Person = {
-    id: number;
-    name: string;
-  };
-}
---
+++ src/types/global.d.ts
@@ -0,0 +1,7 @@
+declare type NameMap = {
+  [key: string]: string;
+};
+declare type Person = {
+  id: number;
+  name: string;
+};
